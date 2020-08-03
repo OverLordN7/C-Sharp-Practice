c096c105f3af491d5fea3a2dce5eff6dacf15ab2
OverLordN7 <arifdjanovsarvar@gmail.com>
2020-08-03 21:12:57 +0500

    Arithmetic expressions. task 1
    task 1 Complete

---
+++ ConsoleApp1/ConsoleApp1/Pre100.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    
+    //Class which converts double number to a double with precision to the nearest hundredths.
+    public class Pre100
+    {
+        
+        double FNumber;
+        double SNumber;
+
+        public Pre100(double value)
+        {
+            this.FNumber = value;
+            this.SNumber = Math.Round(FNumber, 2);
+        }
+        public void Show()
+        {
+            Console.WriteLine("First number: "+FNumber);
+            Console.WriteLine("First number with precision: "+SNumber);  
+        }
+    }
+}
--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -2,11 +2,22 @@
 
 namespace ConsoleApp1
 {
+    
+    //Arithmetic expressions task 1-13
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            /*
+            //Task 1
+            Pre100 obj = new Pre100(100.789);
+            obj.Show();
+            */
+           
+            
+            
+            
         }
     }
 }
