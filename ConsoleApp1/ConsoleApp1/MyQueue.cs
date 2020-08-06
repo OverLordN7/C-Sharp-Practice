3b9da2b040b38ca5837af7fd198179f08f371a9c
OverLordN7 <arifdjanovsarvar@gmail.com>
2020-08-06 11:01:02 +0500

    draft for leetcode interview
    small c# experiments

---
+++ ConsoleApp1/ConsoleApp1/MyQueue.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    public class MyQueue
+    {
+        List<int> stack = new List<int>();
+
+        /** Initialize your data structure here. */
+        public MyQueue() {
+        
+        }
+    
+        /** Push element x to the back of queue. */
+        public void Push(int x) {
+            stack.Insert(0,x);
+        }
+    
+        /** Removes the element from in front of queue and returns that element. */
+        public int Pop() {
+            stack.RemoveAt(0);
+            int result = stack.IndexOf(0);
+            return result;
+        }
+    
+        /** Get the front element. */
+        public int Peek() {
+            int result = stack.IndexOf(0);
+            return result;
+        }
+    
+        /** Returns whether the queue is empty. */
+        public bool Empty() {
+            if(stack.IndexOf(0) !=null){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+    }
+}
--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,8 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ConsoleApp1
 {
-    
+
     //Arithmetic expressions task 1-13
     class Program
     {
@@ -21,8 +22,93 @@ namespace ConsoleApp1
             obj.SetNumber(Convert.ToInt32(Console.ReadLine()));
             Console.WriteLine("You entered: "+ obj.GetNumber());
             */
+            
+            /*
+            //Task 3 
+            int year = 2019;
+            int month = 8;
+            int day = 5;
+            DateTime obj = new DateTime(year,month,day);
+            
+            Console.WriteLine(obj.DayOfWeek.ToString());
+            */
+            
+            /*
+            //Task 4
+             MyQueue obj = new MyQueue();
+                obj.Push(5);
+                int param_2 = obj.Pop();
+                int param_3 = obj.Peek();
+                bool param_4 = obj.Empty();
+                */
+            
 
+            /*
+            //Task 5
+            int n = 64;
+            bool Success = false;
+            if(n==1){
+                Success =  true;
+            }
+            else{
+                if(n%2 !=0){
+                    Success = false;
+                }
+                else{
+                    while(n>=2){
+                        Console.WriteLine(n);
+                        if(n%2 != 0){Success = false;break;}
+                        if(n==2){Success = true;}
+                        n/=2;
+                    }
+                    Console.WriteLine(Success);
+                }
+            
+            }
+            */
 
+            /*
+            //Task 6
+            string dna = "TTTT";
+            char[] DNA = dna.ToCharArray();
+            string FDNA = "";
+
+            for (int i = 0; i < DNA.Length; i++)
+            {
+                switch (DNA[i])
+                {
+                    case 'T':
+                    {
+                        DNA[i] = 'A';
+                        break;
+                    }
+                    case 'A':
+                    {
+                        DNA[i] = 'T';
+                        break;
+                    }
+                    case 'C':
+                    {
+                        DNA[i] = 'G';
+                        break;
+                    }
+                    case 'G':
+                    {
+                        DNA[i] = 'C';
+                        break;
+                    }
+                }
+            }
+
+            FDNA =  new string(DNA);
+            
+                
+            Console.WriteLine(dna);
+            Console.WriteLine(FDNA);
+            */
+            
+            
+            
         }
     }
 }
